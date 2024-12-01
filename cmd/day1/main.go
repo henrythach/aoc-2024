0de3a6ab9d0944ef4d4bf4776ace3b4f5e51c4eb
Henry Thach <thachhao@gmail.com>
2024-12-01 14:19:47 -0500

    TIL I can just use `range lines`

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -17,8 +17,9 @@ func main() {
 	lines := strings.Split(input, "\n")
 	left := make([]int, len(lines))
 	right := make([]int, len(lines))
-	for i := 0; i < len(lines); i++ {
-		if len(lines[i]) == 0 {
+
+	for i, line := range lines {
+		if len(line) == 0 {
 			continue
 		}
 		part := re.Split(lines[i], 2)
